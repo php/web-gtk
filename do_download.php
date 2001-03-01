7a57e4926a98dccf7070a824c7283e1fae7b603d
Andrei Zmievski <andrei@php.net>
2001-03-01 17:13:01 +0000

    Adding first release and download script.

---
+++ do_download.php
@@ -0,0 +1,23 @@
+<?
+if (!isset($HTTP_REFERER)) {
+	$HTTP_REFERER="-";
+}
+
+if (!isset($download_file)
+	|| !file_exists("distributions/$download_file")) {
+	exit("Invalid file requested for download!");
+}
+
+if (!isset($source_site)) {
+	$source_site="gtk.php.net";
+}
+
+header("Location:  http://$source_site/distributions/$download_file");
+
+$log = @fopen("logs/download.log", "a");
+if ($log) {
+	$log_line = sprintf("%s %s %s %s %s", date("H:i:s d-M-Y"), $HTTP_REFERER, $download_file, gethostbyaddr($REMOTE_ADDR), $source_site);
+	fputs($log, "$log_line\n");
+	fclose($log);
+}
+?>
