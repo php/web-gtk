3793374f93d22534448f2b6a919a80538a32eb5a
Frank M. Kromann <fmk@php.net>
2001-03-02 23:10:02 +0000

    Adding install info on WIn2 to faq

--- faq.php
+++ faq.php
@@ -12,10 +12,37 @@
 
 <ul>
 <li><a href="#1">What is PHP-GTK?</a>
+<li><a href="#2">How do I install PHP-GTK on Win32?</a>
 </ul>
 
 <a name="1"><b>What is PHP-GTK?</b></a>
 <dd>blah
+<br>
+<br>
+<a name="2"><b>How do I install PHP-GTK on Win32?</b></a><br>
+Download the latest <a href="http://gtk.php.net/do_download.php?download_file=php-gtk-0.0.1-win32.zip">binaries</a> from gtk.php.net<br>
+The zip file contains all binaries needed to run PHP-GTK. Copy the files to the following locations:<br>
+<br>
+On Windows 98/NT/2000 you will need these files<br>
+<br>
+[php-directory] (c:\php4)<br>
+<dd>php.exe<br>
+<dd>php4ts.dll<br>
+<dd>php_gtk.dll<br>
+<br>
+[Windows directory] (c:\winnt or c:windows)<br>
+<dd>php.ini<br>
+<br>
+[System32 directory] (c:\winnt\system32 or c:\windows\system32)<br>
+<dd>gtk-1.3.dll<br>
+<dd>gdk-1.3.dll<br>
+<dd>gmodule-1.3.dll<br>
+<dd>glib-1.3.dll<br>
+<dd>iconv-1.3.dll<br>
+<dd>gnu-intl.dll<br>
+<br>
+php-gtk has NOT been tested on Windows 95/98<br>
+
 
 </body>
 
