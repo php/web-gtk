5bb73fd055d040015499538cbebd437d87425c3d
Andrei Zmievski <andrei@php.net>
2001-03-02 21:56:56 +0000

    Adding FAQ template.

---
+++ faq.php
@@ -0,0 +1,22 @@
+<html>
+
+<head>
+<title>PHP-GTK FAQ</title>
+</head>
+
+<body bgcolor="#ffffff">
+
+<font face="sans-serif" size=+1>
+<b>PHP-GTK FAQ</b>
+</font>
+
+<ul>
+<li><a href="#1">What is PHP-GTK?</a>
+</ul>
+
+<a name="1"><b>What is PHP-GTK?</b></a>
+<dd>blah
+
+</body>
+
+</html>
