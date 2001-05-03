0a9404f5a520e940a203854453f297385d84ceb8
Colin Viebrock <cmv@php.net>
2001-05-03 18:54:04 +0000

    updates

--- index.php
+++ index.php
@@ -1,47 +1,55 @@
 <?php
-require_once("includes/shared.inc");
 
-common_header();
+$SIDEBAR_DATA = <<< EOF
+<h3>Warning!</h3>
+<P>
+This code is a highly experimental adaptation 
+of the instructions beamed to us by the friendly aliens of Tau 
+Ceti. The API can and will be changing, evolving, and unpredictably 
+morphing into little furry Ewoks, fluorescent killer bunny rabbits, 
+and the knights who say 'Nee'. Use the code at your own risk, 
+as it may cause flooding, polydactism, frogs falling from the 
+sky, and the return of the Inca empire. And it will definitely 
+warp space-time continuum in your local area.
+<P>
+EOF;
+
+commonHeader();
 
 ?>
-<table width="80%" border="0" cellpadding="5" cellspacing="0" height="100%">
-        <tr>
-          <td>
-           <div class="title">PHP-GTK</div>
-            <p class="mtext"> Too often PHP is thought of as only an HTML-embedded 
-              Web scripting language. But it is also a very full-featured general 
-              purpose language that can be used for much more. One of the goals 
-              behind this project was to prove that PHP can be used to write client-side 
-              GUI applications. </p>
-            <blockquote> 
-              <p><b>Warning!</b> This code is a highly experimental adaptation 
-                of the instructions beamed to us by the friendly aliens of Tau 
-                Ceti. The API can and will be changing, evolving, and unpredictably 
-                morphing into little furry Ewoks, fluorescent killer bunny rabbits, 
-                and the knights who say 'Nee'. Use the code at your own risk, 
-                as it may cause flooding, polydactism, frogs falling from the 
-                sky, and the return of the Inca empire. And it will definitely 
-                warp space-time continuum in your local area. </p>
-            </blockquote>
-            <p class="mtext">There is a mailing list for PHP-GTK. To subsribe, 
-              send blank email to <a href="mailto:php-gtk-subscribe@lists.php.net">php-gtk-subscribe@lists.php.net</a>. 
-              The address of the list itself is <a href="mailto:php-gtk@lists.php.net">php-gtk@lists.php.net</a>. 
-              The list is archived at <a href="http://marc.theaimsgroup.com/?l=php-gtk&r=1&w=2">MARC</a>. 
-            </p>
-			<p class="mtext">This extension is still in early beta phase, so not
-			 all the features might work right now. If you have problems, then
-			 your first port of call should be the manual and the mailing list.
-			 Any serious php-gtk related questions should be sent to
-			 <a href="mailto:andrei@php.net">andrei@php.net</a> unless they are
-			 related to the manual where they should be sent to
-			 <a href="mailto:jmoore@php.net">jmoore@php.net</a>. If you have a
-			 question or suggestion for the website you should contact
-			 <a href="mailto:jmoore@php.net">jmoore@php.net</a>.</p>
-          </td>
-        </tr>
-      </table>
+
+<h1>PHP-GTK</h1>
+
+<P>
+Too often PHP is thought of as only an HTML-embedded 
+Web scripting language. But it is also a very full-featured general 
+purpose language that can be used for much more. One of the goals 
+behind this project was to prove that PHP can be used to write client-side 
+GUI applications.
+</P>
+
+<P>
+There is a mailing list for PHP-GTK. To subsribe, 
+send blank email to <?php print_email('php-gtk-subscribe@lists.php.net'); ?>.
+The address of the list itself is <?php print_email('php-gtk@lists.php.net'); ?>. 
+The list is archived at <?php print_link('http://marc.theaimsgroup.com/?l=php-gtk&r=1&w=2', 'MARC'); ?>. 
+</P>
+
+<P>
+This extension is still in early beta phase to not 
+expect everything to work right now. If you have problems then your 
+first port of call should be the manual and the mailing list. Any
+serious php-gtk related questions should be sent to 
+<?php print_email('andrei@php.net'); ?> ... unless
+they are related to the manual where they should be sent 
+to <?php print_email('jmoore@php.net'); ?>. If you have 
+a question or suggestion for the website you should contact 
+<?php print_email('jmoore@php.net'); ?>.
+</P>
+
+
 <?php
 
-common_footer();
+commonFooter();
 
 ?>
--- manual.php
+++ manual.php
@@ -1,29 +1,28 @@
 <?php
-require_once("includes/shared.inc");
 
-common_header();
+commonHeader('Documentation');
 
 ?>
-<table width="80%" border="0" cellpadding="5" cellspacing="0" height="100%">
-        <tr> 
-          <td> 
-            <div class="title">Manual</div>
-            <p class="mtext">The PHP-GTK manual is currently being written by 
-              a team of little easter bunnies. This means the manual is not here 
-              right now but you can see the latest development version at <a href="http://www.phpuk.org/gtk/">http://www.phpuk.org/gtk/</a></p>
-            <p class="mtext">If you have some time to spare then please help us 
-              with the manual. We need lots of people to document all of the objects, 
-              whether you want to write the docs or are willing to check the example 
-              code and comment on the content of the documentation please drop 
-              me a line at <a href="mailto:jmoore@php.net">jmoore@php.net</a>. 
-              Any help is gratefully received.</p>
-            <p class="mtext">&nbsp;</p>
-            <blockquote>&nbsp;</blockquote>
-          </td>
-        </tr>
-      </table>
+<h1>Documentation</h1>
+
+<P>
+The PHP-GTK manual is currently being written by 
+a team of little easter bunnies.  This means the manual is not here 
+right now but you can see the latest development version at 
+<?php print_link('http://www.phpuk.org/gtk/'); ?>.
+</P>
+
+<P>
+If you have some time to spare then please help us 
+with the manual; we need lots of people to document all of the objects!
+Whether you want to write the documentaion, or are willing to check the example 
+code and comment on the content of the documentation, please drop 
+us a line at <? print_email('jmoore@php.net'); ?>.  Any help is gratefully
+received.
+</P>
+
 <?php
 
-common_footer();
+commonFooter();
 
 ?>
