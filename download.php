12c2f90deaa19e644b4a6afe6601d51d0f906535
Frank M. Kromann <fmk@php.net>
2001-05-08 19:11:38 +0000

    Changed link to new windows binary file

--- download.php
+++ download.php
@@ -22,8 +22,8 @@ of GTK+ v1.2.x from <?php print_link('ftp://ftp.gtk.org/pub/gtk/v1.2/'); ?>.
 <LI><?php print_link('http://gtk.php.net/do_download.php?download_file=php-gtk-0.0.4.tar.gz', 
 	'php-gtk-0.0.4 Source'); ?> - 5-May-2001<br>
 
-<LI><?php print_link('http://gtk.php.net/do_download.php?download_file=php-gtk-0.0.3-win32.zip', 
-	'php-gtk-0.0.3 Windows and PHP Binary'); ?> - 20-Mar-2001<br>
+<LI><?php print_link('http://gtk.php.net/do_download.php?download_file=php-gtk-0.0.4-win32.zip', 
+	'php-gtk-0.0.4 Windows and PHP Binary'); ?> - 8-May-2001<br>
 
 </UL>
 
