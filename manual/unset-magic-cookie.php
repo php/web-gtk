6e92df5d50308c0e33adbdfad0d2769c517aa424
Colin Viebrock <cmv@php.net>
2001-08-09 20:15:41 +0000

    grrr...

--- manual/unset-magic-cookie.php
+++ manual/unset-magic-cookie.php
@@ -1,9 +1,11 @@
 <?
-SetCookie("MAGIC_COOKIE");
+SetCookie('MAGIC_COOKIE', '', 0);
+unset($MAGIC_COOKIE);
+
 commonHeader('Magic Cookie');
 
 echo '<h1>Magic Cookie</h1>';
-echo "<P><b>Cookie unset</b>.</P>";
+echo '<P><b>Cookie unset</b>.</P>';
 
 commonFooter();
 
