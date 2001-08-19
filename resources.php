b96694888b83992d579257f3f15cebcad91a9f5a
Andrei Zmievski <andrei@php.net>
2001-08-19 15:30:26 +0000

    Update resources.

--- resources.php
+++ resources.php
@@ -18,11 +18,12 @@ please <? print_email('gtk-webmaster@php.net', 'email the webmaster'); ?>.
 <LI><? print_link('http://gtk.php-coder.net/', 'GTK at PHP-Coder'); ?><BR>
 This site provides current news, some basic tutorials and example applications.<br>
 
-<LI><? print_link('http://www.phpgtk.com/', 'PHPGTK.com'); ?><BR>
-A Windows install and PHP-GTK news site.<br>
-
 <LI><? print_link('http://developer.gnome.org/doc/API/gtk/gtkobjects.html', 'GTK+ Reference Manual'); ?><BR>
 The GTK+ manual with a list of all widgets and their corresponding functions/attributes.<BR>
+
+<LI><? print_link('http://mail.gnome.org/mailman/listinfo/gtk-list', 'GTK+ mailing list archives'); ?><BR>
+This list might be useful if you have a certain GTK+ question or problem that
+PHP-GTK list has no information about.<BR>
 
 </UL>
 
@@ -35,28 +36,27 @@ The GTK+ manual with a list of all widgets and their corresponding functions/att
 There are various mailing lists for PHP-GTK.
 </p>
 <p>
-The <b>php-gtk-general</b> list is used for general discussion about using PHP-GTK. To subscribe,
+The <b>php-gtk-general</b> list is meant for general discussion about using PHP-GTK. To subscribe,
 send blank email to <?php print_email('php-gtk-general-subscribe@lists.php.net'); ?>.
 The address of the list itself is <?php print_email('php-gtk-general@lists.php.net'); ?>.
-The list is archived at <?php print_link('http://marc.theaimsgroup.com/?l=php-gtk-general&r=1&w=2', 'MARC'); ?>.
-</P>
+The list is archived at <?php print_link('http://marc.theaimsgroup.com/?l=php-gtk-general', 'MARC');
+?>.
+</p>
 <p>
-The <b>php-gtk-dev</b> list is used for discussion about the development of PHP-GTK itself. To subscribe,
+The <b>php-gtk-dev</b> list is discussion about the development of PHP-GTK itself. To subscribe,
 send blank email to <?php print_email('php-gtk-dev-subscribe@lists.php.net'); ?>.
 The address of the list itself is <?php print_email('php-gtk-dev@lists.php.net'); ?>.
-The list is archived at <?php print_link('http://marc.theaimsgroup.com/?l=php-gtk-dev&r=1&w=2', 'MARC'); ?>.
-</P>
+The list is archived at <?php print_link('http://marc.theaimsgroup.com/?l=php-gtk-dev', 'MARC'); ?>.
+</p>
 <p>
-The <b>php-gtk-cvs</b> list is sent all cvs commits to PHP-GTK and related projects. To subscribe,
+The <b>php-gtk-cvs</b> list has all the CVS commits to PHP-GTK and related projects posted to it automatically. To subscribe,
 send blank email to <?php print_email('php-gtk-cvs-subscribe@lists.php.net'); ?>.
 The address of the list itself is <?php print_email('php-gtk-cvs@lists.php.net'); ?>.
-The list is archived at <?php print_link('http://marc.theaimsgroup.com/?l=php-gtk-cvs&r=1&w=2', 'MARC'); ?>.
-</P>
+The list is archived at <?php print_link('http://marc.theaimsgroup.com/?l=php-gtk-cvs', 'MARC'); ?>.
+</p>
 <p>
 The old <b>php-gtk</b> list is still archived at <?php print_link('http://marc.theaimsgroup.com/?l=php-gtk&r=1&w=2', 'MARC'); ?>.
-These archives will be moved to the php-gtk-general list archives at <?php print_link('http://marc.theaimsgroup.com/', 'MARC');?> 
-in the next few days.
-</P>
+</p>
 
 
 
