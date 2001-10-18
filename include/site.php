195a6677035553287d2c3c34f53b43c9975d44b9
Andrei Zmievski <andrei@php.net>
2001-10-18 19:23:01 +0000

    Forgot to add site.php.

---
+++ include/site.php
@@ -0,0 +1,37 @@
+<?php
+
+# http://www.unicode.org/unicode/onlinedat/languages.html
+$LANGUAGES = array(
+    'en' => 'English',
+    'pt_BR' => 'Brazilian Portuguese',
+    'bg' => 'Bulgarian',
+    'ca' => 'Catalan',
+    'zh' => 'Chinese',
+    'cs' => 'Czech',
+    'da' => 'Danish',
+    'nl' => 'Dutch',
+    'fi' => 'Finnish',
+    'fr' => 'French',
+    'de' => 'German',
+    'el' => 'Greek',
+    'hu' => 'Hungarian',
+    'in' => 'Indonesian',
+    'it' => 'Italian',
+    'ja' => 'Japanese',
+    'kr' => 'Korean', # this should be 'ko'. its wrong in phpdoc.
+    'lv' => 'Latvian',
+    'no' => 'Norwegian',
+    'pl' => 'Polish',
+    'pt' => 'Portuguese',
+    'ro' => 'Romanian',
+    'ru' => 'Russian',
+    'sk' => 'Slovak',
+    'sl' => 'Slovenian',
+    'es' => 'Spanish',
+    'sv' => 'Swedish',
+    'th' => 'Thai',
+    'tr' => 'Turkish',
+    'uk' => 'Ukranian',
+);
+
+?>
