ebe9ffd35571689bfd5094b3d6086b54fda6ad49
Ben Ramsey <ramsey@php.net>
2004-07-20 18:15:33 +0000

    Moving certain information into the page sidebar, while leaving the News in the main content area of the page.

--- index.php
+++ index.php
@@ -1,29 +1,9 @@
 <?php
+/* $Id$ */
 
-/*
+
 $SIDEBAR_DATA = <<< EOF
-<h3>Warning!</h3>
-<P>
-This code is a highly experimental adaptation 
-of the instructions beamed to us by the friendly aliens of Tau 
-Ceti.  The API can -- and will -- be changing, evolving, and unpredictably 
-morphing into little furry Ewoks, fluorescent killer bunny rabbits, 
-and the knights who say 'Ni'.  Use the code at your own risk, 
-as it may cause flooding, polydactism, frogs falling from the 
-sky, and the return of the Inca empire.
-</P>
-
-It will definitely warp space-time continuum in your local area.
-<P></P>
-EOF;
-*/
-
-commonHeader();
-
-?>
-
-<h1>PHP-GTK</h1>
-
+<h2>PHP-GTK</h2>
 <P>
 PHP-GTK is an extension for PHP programming language that implements language
 bindings for GTK+ toolkit. It provides an object-oriented interface to GTK+
@@ -31,6 +11,40 @@ classes and functions and greatly simplifies writing client side cross-platform
 GUI applications.
 </P>
 
+<h2>Resources</h2>
+<P>
+EOF;
+
+$SIDEBAR_DATA .= 'Check out our ' . print_link('/resources.php', 'Resources page');
+
+$SIDEBAR_DATA .= <<< EOF
+for links to PHP-GTK related sites, instructions on the PHP-GTK mailing lists, 
+and other PHP-GTK resources. Also, please try to use the 
+<strong>#php-gtk</strong> channel on Freenode IRC - the more people there, the 
+better for the community.
+</P>
+
+<h2>Contact</h2>
+<P>
+If you have problems or questions, your first point of contact should be the
+manual and the <strong>php-gtk-general</strong> mailing list.
+</P>
+
+<P>
+Any serious PHP-GTK related questions should be sent to 
+EOF;
+
+$SIDEBAR_DATA .= print_email('andrei at php dot net');
+$SIDEBAR_DATA .= ', unless they are related to the manual, in which case they';
+$SIDEBAR_DATA .= ' should be sent to ' . print_email('jmoore at php dot net');
+$SIDEBAR_DATA .= ' If you have a question or suggestion for the website, you';
+$SIDEBAR_DATA .= ' should contact ';
+$SIDEBAR_DATA .= print_email('php-gtk-webmaster at lists dot php dot net');
+$SIDEBAR_DATA .= '.</P>';
+
+commonHeader();
+
+?>
 <h1>News</h1>
 
 <p>
@@ -102,33 +116,7 @@ piece of software using PHP-GTK, point your browser to
 <? print_link('/apps/', 'Applications'); ?> area and add it to the list.
 </p>
 
-
-<h1>Resources</h1>
-
-<P>
-Check out our <? print_link('/resources.php', 'Resources page'); ?> for links to
-PHP-GTK related sites, instructions on the PHP-GTK mailing lists, and other
-PHP-GTK resources. Please try to use <strong>#php-gtk</strong> channel on EFNet
-or irc.openprojects.net - the more people are there, the better for community.
-</P>
-
-
-
-<h1>Contact</h1>
-
-<P>
-If you have problems or questions then your first point of contact should be the
-manual and the mailing list.
-</P>
-<P>
-Any serious PHP-GTK related questions should be sent to 
-<?php print_email('andrei at php dot net'); ?> ... unless
-they are related to the manual, in which case they should be sent 
-to <?php print_email('jmoore at php dot net'); ?>.  If you have 
-a question or suggestion for the website, you should contact 
-<?php print_email('php-gtk-webmaster at lists dot php dot net'); ?>.
-</P>
-
+<?hdelim()?>
 
 <?php
 
