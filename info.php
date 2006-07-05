b90e1ba26a154683a4a0581759ad3ab78fe5e384
Steph Fox <sfox@php.net>
2006-07-05 04:22:14 +0000

    Update to use our cookie

--- info.php
+++ info.php
@@ -1,6 +1,6 @@
 <?php
 
-if (!isset($_COOKIE['MAGIC_COOKIE'])) { /* remember to change this, assuming everything works */
+if (!isset($_COOKIE['GTK'])) { /* remember to change this, assuming everything works */
 	header("Location: {$_SERVER['HTTP_REFERER']}");
 	exit;
 }
