e8ffe22eee42ff1f950260193c787bf37c315b94
Steph Fox <sfox@php.net>
2006-07-05 06:34:59 +0000

    Prettification (just in case it suddenly starts working ;)

--- admin-login.php
+++ admin-login.php
@@ -7,6 +7,7 @@ require_once('config.inc');
 require_once('cvs-auth.inc');
 
 commonHeader("Administration Login");
+print "<br />\n";
 
 if (!isset($_COOKIE['GTK'])) {
 	if (isset($_POST['submit']) && isset($_POST['pass'])) {
@@ -14,15 +15,20 @@ if (!isset($_COOKIE['GTK'])) {
 	} else {
 		$user = null;
 ?>
-
-		<form action = "<?php echo $_SERVER['PHP_SELF'];?>" method = 'POST'>
-		User: <input type = 'text' name = 'user' value = "<?php echo $user;?>">
-		<br />
-		Pass: <input type = 'password' name = 'pass' value = '' size = '12'>
-		<br />
-		<br />
-		<input type = 'submit' name = 'submit'>
-		</form>
+	<h1>Administration Login</h1>
+	<form action = "<?php echo $_SERVER['PHP_SELF'];?>" method = 'POST'>
+	<table border='0' cellpadding='3' bgcolor='#e0e0e0' width=<?php echo isset($SIDEBAR_DATA) ? '50%' : '40%'; ?>>
+	<tr>
+	<td>User name:</td>
+	<td><input type = 'text' name = 'user' value = "<?php echo $user;?>"><br /></td>
+	<tr>
+	<td>Password:</td>
+	<td><input type = 'password' name = 'pass' value = '' size = '12'><br /></td>
+	<tr>
+	<td colspan='2' align='right'><input type = 'submit' name = 'submit'></td>
+	</tr>
+	</table>
+	</form>
 
 <?php
 	}
@@ -31,19 +37,18 @@ if (!isset($_COOKIE['GTK'])) {
 	unset($pass);
 	print("
 		<h1>You're logged in</h1>
-		Where do you want to go today?
-		<br />
 		<br />
 		<ul>
-			<li><a href='/apps/admin-apps.php'>Application Administration</a>
-			<br />
-			<li><a href='/manual/admin-notes.php'>Notes Administration</a>
-			<br />
-			<li><a href='admin-logout.php'>Log out</a>
+			<li><a href='/apps/admin-apps.php'>Application Administration</a><br /></li>
+			<li><a href='/manual/admin-notes.php'>Notes Administration</a><br /></li>
+			<li><a href='info.php'>Check phpinfo()</a><br /></li>
+			<li><a href='admin-logout.php'>Log out</a></li>
 		</ul>
 	");
 }
 
+print stretchPage(11);
+print "&nbsp;</div>\n";
 commonFooter();
 
 ?>
