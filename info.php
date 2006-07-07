778f5be5f46c6cea87cdcfae64fc1602ff3853a0
Steph Fox <sfox@php.net>
2006-07-07 15:31:03 +0000

    oops

--- info.php
+++ info.php
@@ -3,7 +3,8 @@
 require_once('cvs-auth.inc');
 
 if (!get_user()) {
-	$referrer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : $_SERVER['REQUEST_URI'];
+	$referrer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], '/'));
+
 	header("Location: $referrer");
 	exit;
 }
