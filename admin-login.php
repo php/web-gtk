f167ae5741f1952f5e7dc61b402955c047f26372
Steph Fox <sfox@php.net>
2006-07-10 20:13:19 +0000

    I'm working completely blind here, so let's see if this helps any.

--- admin-login.php
+++ admin-login.php
@@ -12,9 +12,10 @@ if (!isset($_COOKIE['PHP-GTK'])) {
 		verify_password($_POST['user'], $_POST['pass']);
 	} else {
 		$user = null;
+		$self = substr($_SERVER['PHP_SELF'], 0, strpos($_SERVER['PHP_SELF'], '.php') + 4);
 ?>
 	<h1>Administration Login</h1>
-	<form action = "<?php echo $_SERVER['PHP_SELF'];?>" method = 'POST'>
+	<form action = "<?php echo $self ?>" method = 'POST'>
 	<table border='0' cellpadding='3' bgcolor='#e0e0e0' width=<?php echo isset($SIDEBAR_DATA) ? '50%' : '40%'; ?>>
 	<tr>
 	<td><br />User name:</td>
