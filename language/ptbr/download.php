8b2878ad10afe150fb6817a661fdafb3302b0373
Alexandre Pereira Bühler <buhlerax@php.net>
2015-12-28 01:56:50 -0200

    Made adjustments in translation

--- language/ptbr/download.php
+++ language/ptbr/download.php
@@ -11,7 +11,7 @@ define('TITLE2','PHP-GTK 1 para MS-Windows');
 define('TITLE3','PHP-GTK 2 código fonte baixado pelo GitHub');
 define('TEXT1','<b>Observações:<br/>Versão do GTK+:</b> PHP-GTK 2 atualmente suporta GTK+ 2.24.10 ou maior. Você pode obter a última versão estável do GTK+ 2.x em <a href="http://ftp.gtk.org/pub/gtk/" target="_blank">http://ftp.gtk.org/pub/gtk/</a>.
 <br/><b>Versão do PHP:</b> PHP-GTK 2 requer PHP 5.1.x até PHP 5.5.x
-<br/><b>Versão Alfa e Beta do PHP-GTK:</b> Seu uso em produção é a seu próprio risco. Nós não assumimos qualquer dano que possa ocorrer.</br> Por favor, reporte bugs para php-gtk-dev@lists.php.net.');
+<br/><b>Versão Alfa e Beta do PHP-GTK:</b> O uso desta versão é a seu próprio risco. Nós não assumimos quaisquer danos que possam ocorrer na sua aplicação ou sistema operacional.</br><b> Por favor, reporte bugs para php-gtk-dev@lists.php.net.</b>');
 define('TEXT2','<b>Observações:<br/>Versão do GTK+:</b> PHP-GTK 1 atualmente suporta GTK+ 1.2.6 ou maior. Você pode obter a última versão estável do GTK+ 1.2.x em <a href="http://ftp.gtk.org/pub/gtk/v1.2/" target="_blank">http://ftp.gtk.org/pub/gtk/v1.2/</a>.
 <br/><b>Versão do PHP:</b> PHP-GTK 1 requer PHP 4.0.5 ou maior, e versões 1.0.1 e acima requerem PHP 4.3.x ou maior.');
 define('TEXT3','<b>Observações:</b><br/>Alternativamente, você pode obter a versão mais recente do PHP-GTK diretamente do GitHub. ');
@@ -32,6 +32,6 @@ define('TH6','Estável?');
 define('TH7','Lançamento');
 define('TH8','Downloads');
 define('FOOTER','Se você tiver problemas ou dúvidas, o seu primeiro ponto de contato deve ser o <a href="manual/en/html/index.html" target="_blank">manual</a> e a <a href="null.php?language=pt-BR">lista de discussão php-gtk-geral</a>');
-define('COPYRIGHT','Direitos autorais © 2001-2015 Equipe PHP-GTK. Todos os direitos reservados.*Último update:  Sábado Dezembro 12 02:28:12 2015 UTC');
+define('COPYRIGHT','Direitos autorais © 2001-2015 Equipe PHP-GTK. Todos os direitos reservados.*Último update:  Sábado Dezembro 28 01:54:12 2015 UTC');
 define('LANGUAGE',"Idioma");
 ?>
