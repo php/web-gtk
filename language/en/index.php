012e726fa32b025a695790872bf2fd8ca2827de9
Alexandre Pereira Bühler <buhlerax@php.net>
2016-06-20 22:13:03 -0300

    We made small changes in the translation by suggestion: Majid Fouladpour

--- language/en/index.php
+++ language/en/index.php
@@ -1,8 +1,8 @@
 <?php
 define('DEFAULTHEADER', '"en":PHP-GTK:"PHP-GTK is an extension for the PHP programming language that implements language bindings for GTK+.":"PHP-GTK,PHP,GTK"');
 define('DEFAULTHEADER1',"Building Desktop Applications in PHP with PHP-GTK");
-define('DEFAULTHEADER2',"You can to send any serious questions about PHP-GTK to php-gtk-dev@lists.php.net, but the questions about PHP-GTK MANUAL you must send them to php-gtk-doc@lists.php.net.
-<br/>Now, if you have any question or suggestion for our website you can send them to php-gtk-webmaster@lists.php.net. Thank you!");
+define('DEFAULTHEADER2',"You can send any serious questions about PHP-GTK to php-gtk-dev@lists.php.net, but the questions about PHP-GTK MANUAL should be sent to php-gtk-doc@lists.php.net.
+<br/>If you have any question or suggestion for our website you can send them to php-gtk-webmaster@lists.php.net. Thank you!");
 define('DEFAULTMENU','Home:Download:Documentation:Applications:FAQ:Changelog:Resources:Wiki:Screenshots');
 define('DEFAULTMENULEFT','News:Roadmap:To help<br/>the project');
 define('WELCOME','Welcome, Learn a little about php-gtk');
@@ -10,10 +10,10 @@ define('TITLE1','What is PHP-GTK?');
 define('TITLE2','History');
 define('TEXT1','PHP-GTK is an extension for the PHP programming language that implements language bindings for GTK+. It provides an object-oriented interface to GTK+ classes and functions and greatly simplifies writing client-side cross-platform GUI applications.');
 define('TEXT2','The PHP-GTK was established in March 2001 by Andrei Zmievski an uzbek who lives and works in the United States. As well as many projects in free software, it also started with an initial motivation a little peculiar, "I did it because I wanted to see if it was possible" in the author’s words who inspired so much in another existing project, the PyGTK (link between languages ​​between Python and GTK). Andrei Zmievski visited Brazil in 2002, during the III International Free Software Forum, which made some presentations.');
-define('TEXT3','The PHP-GTK is a "language binding", ie it is a link between two existing languages, PHP language and GTK library. So PHP-GTK is the PHP itself, with more features. The PHP-GTK is the first extension of the PHP language that lets you write client-side applications with GUI (Graphical User Interface). It was written in part to prove that PHP is a language and full of great purposes. The junction between PHP and TGK tool creates an independent platform that runs on both Linux and Windows environments.');
+define('TEXT3','The PHP-GTK is a "language binding", ie it is a link between two existing languages, PHP language and GTK library. So PHP-GTK is the PHP itself, with more features. The PHP-GTK is the first extension of the PHP language that lets you write client-side applications with GUI (Graphical User Interface). It was written in part to prove that PHP is a language and full of great purposes. The junction between PHP and GTK tool creates an independent platform that runs on both Linux and Windows environments.');
 define('TEXT4','Using PHP-GTK, you create an application that has connectivity to the server (database, access to files, etc.), like all other programs written in PHP, but that because of running on the client machine, also has full access to resources of this (run applications, write local files and access devices). For this purpose, PHP-GTK needs to be installed on each client machine that will run a PHP-GTK application.<b> Source: http://www.php-gtk.com.br/home.</b>');
 define('FOOTER','If you have problems or questions, your first point of contact should be the <a href="manual/en/html/index.html" target="_blank">manual</a> and the <a href="null.php?language=en-US">php-gtk-general mailing list.</a>');
-define('COPYRIGHT','Copyright © 2001-2015 PHP-GTK Team. All rights reserved.*Last updated:  Sunday December 06 06:40:12 2015 UTC');
+define('COPYRIGHT','Copyright © 2001-2015 PHP-GTK Team. All rights reserved.*Last updated:  Monday July 21 00:00:00 2015 UTC');
 define('LANGUAGE',"Language");
 
 ?>
