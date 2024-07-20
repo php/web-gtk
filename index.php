3215dadeae6247392650b93fef4695fed403d32d
jim winstead <jimw@trainedmonkey.com>
2024-07-19 17:02:38 -0700

    Remove CSS animations (ugh)

--- index.php
+++ index.php
@@ -10,10 +10,10 @@ defaultheader($header[0],$header[1],$header[2],$header[3]);
 ?>
 <body>
 <?php defaulttop(); ?>
-<header class="w3-container w3-teal w3-animate-opacity">
+<header class="w3-container w3-teal">
   <h1><?php echo WELCOME;?></h1>
 </header>
-<div class="w3-center w3-animate-opacity">    
+<div class="w3-center">    
   <div class="w3-row"><br/>
     <article class="w3-col m10 w3-container">
     <h1><?php echo TITLE1;?></h1>
@@ -32,14 +32,14 @@ defaultheader($header[0],$header[1],$header[2],$header[3]);
 <p class="w3-justify"><?php echo TEXT4;?></p>
 </article>
     <aside>
-    <nav class="w3-col m2 w3-light-grey w3-animate-opacity">
+    <nav class="w3-col m2 w3-light-grey">
 <?php defaultmenuright();?>
    </nav>
    </aside>
     
   </div>
 </div>
-<footer class="w3-container w3-small w3-teal w3-animate-opacity">
+<footer class="w3-container w3-small w3-teal">
   <p><?php echo FOOTER;?></p>
 </footer>
 
